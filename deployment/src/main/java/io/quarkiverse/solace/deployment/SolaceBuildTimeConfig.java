a99f70373258e5ed66ea9e4bb037561f5084a795
Clement Escoffier <clement.escoffier@gmail.com>
2023-10-30 11:56:24 +0100

    Initial verison of the Solace dev service

--- deployment/src/main/java/io/quarkiverse/solace/deployment/SolaceBuildTimeConfig.java
+++ deployment/src/main/java/io/quarkiverse/solace/deployment/SolaceBuildTimeConfig.java
@@ -1,9 +1,11 @@
 package io.quarkiverse.solace.deployment;
 
+import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
+import io.smallrye.config.WithParentName;
 
 @ConfigMapping(prefix = "quarkus.solace")
 @ConfigRoot(phase = ConfigPhase.BUILD_TIME)
@@ -17,6 +19,22 @@ public interface SolaceBuildTimeConfig {
      * Health configuration.
      */
     HealthConfig health();
+
+    /**
+     * Default Dev services configuration.
+     */
+    @WithParentName
+    DevServiceConfiguration defaultDevService();
+
+    @ConfigGroup
+    public interface DevServiceConfiguration {
+        /**
+         * Configuration for DevServices
+         * <p>
+         * DevServices allows Quarkus to automatically start Solace in dev and test mode.
+         */
+        DevServicesConfig devservices();
+    }
 
     /**
      * Metrics configuration.
